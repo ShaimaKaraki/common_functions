664e544e17d5b2c5c7e901c4f3a6f447807de469
ShaimaKaraki <shaimakaraki@gmail.com>
2020-10-22 11:21:49 +0300

    bootstrap table classes

--- lib/common_functions.rb
+++ lib/common_functions.rb
@@ -1,35 +1,49 @@
-class Button
-  def self.primary_btn
+class Btn
+  def self.btn_primary
     "btn btn-primary"
   end
 
-  def self.primary_btn_sm
+  def self.btn_primary_sm
     "btn btn-primary btn-sm"
   end
 
-  def self.success_btn
+  def self.btn_success
     "btn btn-success"
   end
 
-  def self.success_btn_sm
+  def self.btn_success_sm
     "btn btn-success btn-sm"
   end
 
-  def self.danger_btn
+  def self.btn_danger
     "btn btn-danger"
   end
 
-  def self.danger_btn_sm
+  def self.btn_danger_sm
     "btn btn-danger btn-sm"
   end
 end
 
-class DateTime
+class DTHelper
   def self.print_date(timestamp)
     timestamp.strftime("%F")
   end
 
   def self.print_date_time(timestamp)
     timestamp.strftime("%F %T")
+  end
+end
+
+class Table
+  def self.table_bordered
+    "<table class='table table-bordered'>".html_safe
+  end
+
+  def self.table_striped
+    "<table class='table table-striped'>".html_safe
+  end
+
+  def self.table_bordered_striped
+    "<table class='table table-bordered table-striped'>".html_safe
   end
 end
